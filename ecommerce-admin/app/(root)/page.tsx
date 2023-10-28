65d50f2c250f30783acdd064f1b106e8fc4825fc
cjephuneh <calebjephuneh@gmail.com>
2023-10-28 16:10:37 +0300

    clerk_intergration

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
-    <div>
+    <div className='p-4 flex flex-col '>
       hello Admin dashboard
+      <Button size='sm'> Click me </Button>
     </div>
   )
 }
