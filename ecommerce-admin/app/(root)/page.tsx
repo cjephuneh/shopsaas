3c9500d5873ef76ad3d6987004370c547f028de7
cjephuneh <calebjephuneh@gmail.com>
2023-10-28 17:05:10 +0300

    wuehh

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -2,11 +2,14 @@ import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 
 
-export default function Home() {
+const SetupPage = () =>{
   return (
     <div className='p-4 flex flex-col '>
-      hello Admin dashboard
+      This is a protected route!
       <Button size='sm'> Click me </Button>
     </div>
   )
 }
+
+
+export default SetupPage
